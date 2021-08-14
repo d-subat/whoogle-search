c68c65d4ae8887fc318ca2714467b9abe1295ca9
ds <0x2012@gmail.com>
2021-08-14 13:24:09 +0200

    set to unsafe inline

--- app/__init__.py
+++ app/__init__.py
@@ -67,12 +67,12 @@ if not translate_url.startswith('http'):
     translate_url = 'https://' + translate_url
 app.config['TRANSLATE_URL'] = translate_url
 
-app.config['CSP'] = 'default-src \'self\' aggl.herokuapp.com aggl.herokuapp.com/search;' \
+app.config['CSP'] = 'default-src \'self\' \'unsafe-inline\' aggl.herokuapp.com ;' \
                     'frame-src ' + translate_url + ';' \
                     'manifest-src \'self\';' \
                     'img-src \'self\' data:;' \
-                    'font-src \'self\' \'unsafe-inline\' aggl.herokuapp.com aggl.herokuapp.com/search fonts.gstatic.com;' \
-                    'style-src \'self\' \'unsafe-inline\' fonts.googleapis.com;' \
+                    'font-src \'self\' \'unsafe-inline\' aggl.herokuapp.com;' \
+                    'style-src \'self\' \'unsafe-inline\' ;' \
                     'script-src \'self\';' \
                     'media-src \'self\';' \
                     'connect-src \'self\';' \
